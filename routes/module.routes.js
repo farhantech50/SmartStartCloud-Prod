cb5ec5ec4d1f3eaead44981e73174645bc028ce0
farhantech50 <farhantech50@gmail.com>
2024-11-13 02:45:15 +0600

    first commit

--- routes/module.routes.js
+++ routes/module.routes.js
@@ -1,13 +1,20 @@
-import express from "express"
-import {newAssignment,getAssignment} from '../controllers/module.controller.js'
+import express from "express";
+import {
+  newAssignment,
+  getAssignment,
+} from "../controllers/module.controller.js";
 const router = express.Router();
-import {authenticate} from '../middlewares/protect.js'
-import { deleteAssignment, newAssignmentManual, updateAssignment } from "../controllers/assignment.controllers.js";
+import { authenticate } from "../middlewares/protect.js";
+import {
+  deleteAssignment,
+  newAssignmentManual,
+  updateAssignment,
+} from "../controllers/assignment.controller.js";
 
 router.use(authenticate);
-router.post("/newAssignment",newAssignmentManual);
+router.post("/newAssignment", newAssignmentManual);
 router.put("/updateAssignment/:assignmentID", updateAssignment);
-router.get("/getAssignment/:moduleID/:studentID",getAssignment);
-router.delete('/deleteAssignment/:assignmentID', deleteAssignment)
+router.get("/getAssignment/:moduleID/:studentID", getAssignment);
+router.delete("/deleteAssignment/:assignmentID", deleteAssignment);
 
 export default router;
